468933270b9fc11707e2bbce71d9abc4d3d5fc16
perrishnikov <perrishnikov@gmail.com>
2024-07-29 14:12:29 -0500

    incremental

--- app/scripts/dom.js
+++ app/scripts/dom.js
@@ -1,19 +1,4 @@
-/**  Tabs */
-//todo: defunct
-// function openTab(evt, tabName) {
-//     const tabcontent = document.getElementsByClassName('tabcontent');
-//     for (let i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = 'none';
-//     }
 
-//     const tablinks = document.getElementsByClassName('tablinks');
-//     for (let i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(' active', '');
-//     }
-
-//     document.getElementById(tabName).style.display = 'flex';
-//     evt.currentTarget.className += ' active';
-// }
 
     function displayFileName() {
         const fileInput = document.getElementById('fileInput');
